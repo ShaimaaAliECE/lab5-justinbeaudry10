f88c49a66e7cc9b1a7de51ceab55dfe3695aedd4
justinbeaudry10 <jbeaudr5@uwo.ca>
2021-11-29 14:54:42 -0500

    Part 1 completed

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ let jobsList = require("./jobs.json");
 
 const app = express();
 
+// Get categories mentioned in all the jobs and how many times each category was mentioned
+app.get("/jobCategories", (req, res) => {
+  let categories = {};
+
+  for (let j in jobsList) {
+    for (let c of jobsList[j].categories) {
+      if (!categories[c]) categories[c] = 1;
+      else categories[c]++;
+    }
+  }
+
+  res.json(categories);
+});
+
+// Get all the jobs with a given category (sent as parameter)
+//app.get("/:category", (req, res) => {});
+
+// Get all jobs in a given city (sent in the querystring)
+// app.get("/cityJobs", (req, res) => {
+//   res.json(jobsList);
+// });
+
 app.listen(2000);
